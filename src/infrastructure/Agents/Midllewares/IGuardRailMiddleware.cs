8758e0ee60c458d63ad8f8862c93265d5b61f533
ahmar husain <nucleotidz@hotmail.com>
2026-04-18 11:53:48 +0530

    Fix

--- src/infrastructure/Agents/Midllewares/IGuardRailMiddleware.cs
+++ src/infrastructure/Agents/Midllewares/IGuardRailMiddleware.cs
@@ -6,6 +6,7 @@
 
     using Microsoft.Agents.AI;
     using Microsoft.Extensions.AI;
+  
 
     public interface IGuardRailMiddleware
     {
--- src/infrastructure/Agents/NucleotidzAgent.cs
+++ src/infrastructure/Agents/NucleotidzAgent.cs
@@ -10,19 +10,30 @@
     using Microsoft.Agents.AI;
     using Microsoft.Extensions.AI;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.SemanticKernel.Connectors.Redis;
+
+    using StackExchange.Redis;
 
     internal class NucleotidzAgent(IChatClient chatClient,
         [FromKeyedServices("nucleotidz")] AIAgent agent,
         ITextSearchAdapter textSearchAdapter,
-        IGroundnessDetector groundnessDetector) : INucleotidzAgent
+        IGroundnessDetector groundnessDetector,
+        IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator) : INucleotidzAgent
     {
         public async Task<string> Start(string conversationId, string UserId, string message)
         {
-            var session = await agent.CreateSessionAsync();
+            IConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            var vectorStore = new RedisVectorStore(redis.GetDatabase(), new RedisVectorStoreOptions { EmbeddingGenerator = embeddingGenerator });
+            var builtAgent = agent.AsBuilder().UseAIContextProviders
+                 (
+                 new ChatHistoryMemoryProvider(vectorStore, collectionName: "memory", vectorDimensions: 3072,
+                 session => new ChatHistoryMemoryProvider.State(storageScope: new() { UserId = UserId, SessionId = Guid.NewGuid().ToString() }, searchScope: new() { UserId = UserId }))
+                 ).Build();
+            var session = await builtAgent.CreateSessionAsync();
             session.StateBag.SetValue("conversationId", conversationId);
             session.StateBag.SetValue("UserId", UserId);
-            var response = await agent.RunAsync(message, session);
+            var response = await builtAgent.RunAsync(message, session);
             return response.Text;
-        }       
+        }
     }
 }
